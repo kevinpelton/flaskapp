a156f53d68629ae93fe74b00a3b9b48a9c7bbe99
Kevin Pelton <kjpelton@yahoo.com>
2018-04-23 13:35:42 -0400

    added app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn OpenShift',
+        'description': u'Need to find a good tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/')
+def hello_world():
+    return 'Flask in Docker'
+
+@app.route('/tasks')
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+if __name__ == '__main__':
+    app.run()
